b274afc0efc4d585fb7c2439cf799cbccd7b02f0
Mohamad Rashid <mohamadrashid@MBPsomtrMohamad.lan>
2020-06-21 23:31:49 +0200

    Install swiftmailer bundle package

--- config/bundles.php
+++ config/bundles.php
@@ -22,4 +22,5 @@ return [
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Sonata\UserBundle\SonataUserBundle::class => ['all' => true],
     Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
